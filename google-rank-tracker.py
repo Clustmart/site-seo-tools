041b1192949924218436e2402fdd9bc61dbe5263
Digital Travel Guide <contact@romaniatravel.guide>
2022-02-05 12:31:26 +0200

    cleaned code

--- google-rank-tracker.py
+++ google-rank-tracker.py
@@ -21,95 +21,17 @@ from email.mime.text import MIMEText
 database = "rank.db"
 
 
-# Mobile user agent strings found on https://deviceatlas.com/blog/mobile-browser-user-agent-strings
-mobile_agent = [
-    # Safari for iOS
-    'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1',
-    # Android Browser
-    'Mozilla/5.0 (Linux; U; Android 4.4.2; en-us; SCH-I535 Build/KOT49H) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30',
-    # Chrome Mobile
-    'Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36',
-    # Firefox for Android
-    'Mozilla/5.0 (Android 7.0; Mobile; rv:54.0) Gecko/54.0 Firefox/54.0',
-    # Firefox for iOS
-    'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_2 like Mac OS X) AppleWebKit/603.2.4 (KHTML, like Gecko) FxiOS/7.5b3349 Mobile/14F89 Safari/603.2.4',
-    # Samsung Browser
-    'Mozilla/5.0 (Linux; Android 7.0; SAMSUNG SM-G955U Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/5.4 Chrome/51.0.2704.106 Mobile Safari/537.36',
-]
-
-# desktop user agent strings. Source: https://deviceatlas.com/blog/list-of-user-agent-strings
-desktop_agent = [
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246',
-    'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9',
-    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36',
-    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1'
-]
-
-# Creating Mobile function with 4 arguments.
-"""
-keyword: Keyword that we are checking that comes from keywords.xls
-sitename: the URL we want to chekc against. Its define when we run the script
-device: Mobile or desktop. Its define when we run the script
-useragent: selected randomly from list when we know device.
-"""
 
 
-def mobile(cursor, now, keyword, sitename, device, useragent):
-    parser = 'html.parser'
-
-    browser = RoboBrowser(history=False, user_agent=useragent, parser=parser)
-
-
-    browser.open('https://www.google.com/search?num=100&q=' + keyword)
-
-    # this is the div that only shows up on mobile device. This might change not sure :/
-    links = browser.find_all("div", {"class": "KJDcUb"})
-
-    # links = browser.find_all("div", {"class": "g"})
-
-    counter = 0
-
-    #print('The user Agent you used was ----> ' + useragent)
-
-    # here is where we count and find our url
-    d = []
-    for i in links:
-        counter = counter + 1
-        #print(i)
-        if i == 1:
-            print(str(i))
-        if sitename in str(i):
-            url = i.find_all('a', href=True)
-            position = "%d" % (counter)
-            rank = "%s" % (url[0]['href'])
-            #now = datetime.date.today().strftime("%d-%m-%Y")
-            keyword = keyword
-            device = device
-            d.append(keyword)
-            d.append(position)
-            d.append(rank)
-            d.append(device)
-            d.append(now)
-            print(keyword, position, rank, device, now)
-            insert = "INSERT INTO rankings (url, keyword, date, rank, device) values ('" + rank + "', '" + keyword + "', '" + now[-7:] + "', " + position + ", '" + device + "')"
-            execute(cursor, insert)
-
-
-
-# desktop function. Exactly the same as mobile with the difference of div that we look for our URLs
+# Creating desktop function with 4 arguments.
 def desktop(cursor, now, keyword, sitename, device, useragent):
     parser = 'html.parser'
 
-    useragent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36'
     url = 'https://www.google.com/search'
-    # cookies = chrome_cookies(url, cookie_file='~/snap/chromium/common/chromium/Default/Cookies')
-    # browser = RoboBrowser(history=False, user_agent=useragent, parser=parser)
     browser = RoboBrowser(history=False, user_agent=useragent, parser=parser)
-    # browser.session.cookies.update(cookies)
     browser.open(url + '?num=100&q=' + keyword)
 
-    # check if the page include a form
+    # TBD: check if the page include a form
 
     # links = browser.find_all("div", {"class": "KJDcUb"})
     # desktop div where URLs are
@@ -123,7 +45,7 @@ def desktop(cursor, now, keyword, sitename, device, useragent):
     logging.debug("links")
     logging.debug(str(links))
     logging.debug("===")
-    # check ig google send other error (e.g. too many accesses)
+    # check if google resonses with error (e.g. too many accesses)
     if str(links) == "[]":
         # check the page content
         links = browser.find_all("html")
@@ -133,6 +55,10 @@ def desktop(cursor, now, keyword, sitename, device, useragent):
         text_file.write(str(links))
         # close file
         text_file.close()
+        # Inform by email
+        msg['Subject']="ERROR: program stopped! google-rank-tracker.py | " + str(now) 
+        msg.attach(MIMEText("ERROR", 'plain'))
+        s.send_message(msg)
         # stop querying google, end programm
         sys.exit(0)
 
@@ -285,12 +211,8 @@ def main():
             logging.info("↓"+str(remaining) + " -- " + str(item) + " / " + keyword[0])
             remaining = remaining - 1
 
-#            if empty3url(cursor, keyword[0]):
-            if device == 'mobile':
-                useragent = random.choice(mobile_agent)
-                mobile(cursor, now, keyword[0], sitename, device, useragent)
             if device == 'desktop':
-                useragent = random.choice(desktop_agent)
+                useragent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36'
                 desktop(cursor, now, keyword[0], sitename, device, useragent)
 
             # remember when keyword was last verified
